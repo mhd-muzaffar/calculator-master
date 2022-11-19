eed4eb6dc4cab703362c4e6b640d1e480fffdbe9
mhd-muzaffar <111858254+mhd-muzaffar@users.noreply.github.com>
2022-11-19 15:21:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/', function(req, res) {
 
 
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
-
